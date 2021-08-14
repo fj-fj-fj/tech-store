b78d21b3477368dbe3eec99c62a91b7d70a6628a
fj-fj-fj <x1x1xx0@protonmail.com>
2021-08-14 19:06:06 +0300

    📦 NEW: add urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from .views import EntryView, HomePageView
 
 app_name = 'task_manager'
-urlpatterns = []
+urlpatterns = [
+    path('main/', HomePageView.as_view(), name='index'),
+    path('entry/', EntryView.as_view(), name='entry'),
+] 
