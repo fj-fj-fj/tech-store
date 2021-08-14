4ad0bc8eab30788912846e724adafb3144085f23
fj-fj-fj <x1x1xx0@protonmail.com>
2021-08-14 19:05:06 +0300

    📦 NEW: define views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import FormView, ListView
 
-# Create your views here.
+from task_manager.forms import EmployeeForm
+from task_manager.models import Employee
+
+
+class HomePageView(ListView):
+    model = Employee
+    template_name = 'task_manager/main.html'
+
+
+class EntryView(FormView):
+    form_class = EmployeeForm
+    template_name = 'task_manager/entry.html'
+    success_url = '/main/'
