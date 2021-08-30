6e822d48732d05f1965d5190755cb26f63226327
fj-fj-fj <x1x1xx0@protonmail.com>
2021-08-30 13:52:03 +0300

    📦 NEW: mixins for core.settings

---
+++ core/settings/mixins.py
@@ -0,0 +1,16 @@
+from typing import Any, Tuple
+
+
+class UtilsDevMixin:
+
+    """Mixin that provides useful features for development."""
+
+    @staticmethod
+    def type_watch(source: str, value: Any) -> Tuple[str, Any]:
+        """
+        Shows extra information about any value:
+        local variables, watched expressions, and exploded items.
+
+        see https://pypi.org/project/snoop/#watch_extras
+        """
+        return f'type({source})', type(value)
